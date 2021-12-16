551ac6ad2f694236bf44fd443e987fc2c2ddb937
Enguerrand DE SMET <desmet.enguerrand@gmail.com>
2021-12-16 18:46:44 +0100

    Fix exemple

--- src/test.cpp
+++ src/test.cpp
@@ -1,78 +1,106 @@
-#include "consoleUtils.hpp"
-
 #include <algorithm>
 #include <cstdlib>
 #include <cstdio>
 #include <ctime>
 #include <iomanip> // for std::setfill & std::setw
 
-#define WAIT_ANY_KEY std::cout << "Press any key to continue..." << std::endl; consoleUtils::getChar();
+#include "consoleUtils.hpp"
+
+#define WAIT_ANY_KEY std::cout << "Press any key to continue..." << std::endl; ConsoleUtils::getChar();
 
 int main() {
-
+	ConsoleUtils::clear();
 	std::cout << "Console test program." << std::endl;
 	WAIT_ANY_KEY
 
 	std::cout << "Test 1: Colors" << std::endl;
 	for (int i = 0; i < 16; i++) {
-		consoleUtils::setColor(static_cast<consoleUtils::Color>(i));
+		ConsoleUtils::setColor(static_cast<ConsoleUtils::Color>(i));
 		std::cout << i << " ";
 	}
-	consoleUtils::resetColors();
+
+	ConsoleUtils::setColor(ConsoleUtils::Color::RED);
+	std::cout << std::endl << "This text is red." << std::endl;
+
+	ConsoleUtils::resetColors();
 	std::cout << std::endl << "You should see numbers 0-15 in different colors." << std::endl;
 	WAIT_ANY_KEY
 	
 	std::cout << "\nTest 2: Background colors\n";
 	for (int i = 0; i < 8; i++) {
-		consoleUtils::setBackgroundColor(static_cast<consoleUtils::BackgroundColor>(i));
+		ConsoleUtils::setBackgroundColor(static_cast<ConsoleUtils::BackgroundColor>(i));
 		std::cout << i;
-		consoleUtils::setBackgroundColor(consoleUtils::BackgroundColor::BLACK);
+		ConsoleUtils::setBackgroundColor(ConsoleUtils::BackgroundColor::BG_BLACK);
 		std::cout << ' ';
 	}
-	consoleUtils::setBackgroundColor(consoleUtils::BackgroundColor::BLACK);
-	consoleUtils::resetColors();
+	ConsoleUtils::setBackgroundColor(ConsoleUtils::BackgroundColor::BG_BLACK);
+	ConsoleUtils::resetColors();
 	std::cout << "\nYou should see numbers 0-7 in different-colored backgrounds.\n";
+
 	WAIT_ANY_KEY
 
-	consoleUtils::clear();
+	std::cout << "\nTest 2 bis: Text and background colors\n";
+	ConsoleUtils::setColor(ConsoleUtils::Color::BLUE);
+	ConsoleUtils::setBackgroundColor(ConsoleUtils::BackgroundColor::BG_WHITE);
+	std::cout << "text in blue on white background";
+	ConsoleUtils::resetColors(); std::cout << std::endl;
+	
+	ConsoleUtils::setColor(ConsoleUtils::Color::LIGHTRED);
+	ConsoleUtils::setBackgroundColor(ConsoleUtils::BackgroundColor::BG_CYAN);
+	std::cout << "text in light red on cyan background";
+	ConsoleUtils::resetColors(); std::cout << std::endl;
+
+	ConsoleUtils::setColor(ConsoleUtils::Color::GREEN);
+	ConsoleUtils::setBackgroundColor(ConsoleUtils::BackgroundColor::BG_YELLOW);
+	std::cout << "text in green on yellow background";
+	ConsoleUtils::resetColors(); std::cout << std::endl;
+
+	WAIT_ANY_KEY
+
+	ConsoleUtils::clear();
 	std::cout << "Test 3: Clear screen" << std::endl;
 	std::cout << "The colors should now be gone." << std::endl;
 	WAIT_ANY_KEY
 
-	consoleUtils::clear();
-	std::cout << "Test 6: Cursor positioning" << std::endl;
-	consoleUtils::setCursorPos(16,6); std::cout << "(16,6)";
-	consoleUtils::setCursorPos(4,3); std::cout << "(4,3)";
-	consoleUtils::setCursorPos(8,8); std::cout << "(8,8)";
+	ConsoleUtils::clear();
+	std::cout << "Test 6: Cursor positioning (uses 0-based coordinates)" << std::endl;
+	ConsoleUtils::setCursorPos(0,1); std::cout << "(0,1)";
+	ConsoleUtils::setCursorPos(16,6); std::cout << "(16,6)";
+	ConsoleUtils::setCursorPos(4,3); std::cout << "(4,3)";
+	ConsoleUtils::setCursorPos(8,8); std::cout << "(8,8)";
 	std::cout << std::endl << "You should see three coordinates in their specified locations." << std::endl;
 	WAIT_ANY_KEY
 
-	consoleUtils::clear();
+	ConsoleUtils::clear();
 	std::cout << "Test 7: Blocking keyboard input" << std::endl;
 	std::cout << "You should be able to move the '@' character with zqsd keys." << std::endl;
 	std::cout << "Hit Space to continue to the next test." << std::endl;
 	std::cout << "Last pressed key:" << std::endl;
 	{	
-		int x = 7; int y = 7; bool spaceHit = false;
+		int x = 7; int y = 7; bool exitLoop = false;
 
-		while (!spaceHit) {
-			consoleUtils::setCursorPos(x,y); std::cout << '@'; // Output player
-			char c = consoleUtils::getChar(); // Get
-			consoleUtils::setCursorPos(19,4); std::cout << c;
-			consoleUtils::setCursorPos(x,y); std::cout << ' '; // Clean
+		while (!exitLoop) {
+			ConsoleUtils::setCursorPos(x,y); std::cout << '@'; // Output player
+			
+			bool special = false;
+			int c = ConsoleUtils::getChar(&special); // Get character
+
+			ConsoleUtils::setCursorPos(18, 3); std::cout << (char)c << " - " << c << (special?" special Char":"             ");
+
+			ConsoleUtils::setCursorPos(x,y); std::cout << ' '; // Clean
 			switch (c) {
 				case 'z': --y; break;
 				case 's': ++y; break;
 				case 'q': --x; break;
 				case 'd': ++x; break;
-				case ' ': spaceHit = true; break;
+				case ' ': exitLoop = true; break;
 				default: break;
 			}
 		}
 	}
 
-	consoleUtils::clear();
-	std::cout << "Test 8:  Non-blocking keyboard input" << std::endl;
+	ConsoleUtils::clear();
+	std::cout << "Test 8: Non-blocking keyboard input" << std::endl;
 	std::cout << "You should be able to move the '@' character with ZQSD keys." << std::endl;
 	std::cout << "Hit Space to continue to the next test." << std::endl;
 	std::cout << "Last pressed key:" << std::endl;
@@ -80,37 +108,37 @@ int main() {
 	{	
 		int x = 7;
 		int y = 7;
-		bool spaceHit = false;
+		bool exitLoop = false;
 		unsigned int cnt = 0;
 
-		consoleUtils::setCursorPos(x,y); std::cout << '@'; // first display
-		while (!spaceHit) {
-			
-			consoleUtils::setCursorPos(8, 5); std::cout << std::setfill('0') << std::setw(8) << cnt; // update cnt
+		ConsoleUtils::setCursorPos(x,y); std::cout << '@'; // first display
 
-			if (consoleUtils::kbhit()) {
-				const char c = consoleUtils::getChar(); // Get character
-				consoleUtils::setCursorPos(x, y); std::cout << ' '; // clean
+		while (!exitLoop) {
+			
+			ConsoleUtils::setCursorPos(7, 4); std::cout << std::setfill('0') << std::setw(8) << cnt; // update cnt
+		
+			if (ConsoleUtils::kbhit()) {
+				bool special = false;
+				int c = ConsoleUtils::getChar(&special); // Get character
+
+				ConsoleUtils::setCursorPos(18, 3); std::cout << (char)c << " - " << c << (special?" special Char":"             ");
+				
+				ConsoleUtils::setCursorPos(x, y); std::cout << ' '; // clean
 				switch (c) {
 					case 'z': --y; break;
 					case 's': ++y; break;
 					case 'q': --x; break;
 					case 'd': ++x; break;
-					case ' ': spaceHit = true; break;
+					case ' ': exitLoop = true; break;
 					default: break;
 				}
-				consoleUtils::setCursorPos(x,y); std::cout << '@'; // display
+				ConsoleUtils::setCursorPos(x,y); std::cout << '@'; // display
 			}
-			// Wanning: this sleep somehow breaks the console print
-			consoleUtils::sleep(1);
 			++cnt;
-
 		}
 	}
 
-	// TODO: fix it as extended ascii not work on linux (use utf-8 unstead)
-
-	consoleUtils::clear();
+	ConsoleUtils::clear();
 	std::cout << "All tests done. Bye!" << std::endl;
 	return 0;
 }
